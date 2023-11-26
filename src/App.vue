3f9b06c2784871724f4cfe33a8fce2f79dd51bfc
WeepingDogel <weepingdogel@gmail.com>
2023-11-26 15:35:54 +0800

    Layout of the page.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <script lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import Button from "primevue/button";
 
 export default {
-  
-}
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    Button
+  },
+};
 </script>
 
 <template>
-
+  <div class="container">
+    <div class="content-division">
+      <div class="slide-division">
+        
+      </div>
+      <div class="page-division">
+        <img src="./assets/logo/logo.png" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background-color: cadetblue;
+}
+
+.content-division {
+  height: 100vh;
+  background-color: blueviolet;
+  display: flex;
+  flex-direction: row;
+}
+
+.slide-division {
+  width: 20vw;
+  background-color: rgba(222, 184, 135, 0.45);
+}
+
+.page-division {
+  width: 80vw;
+  background-color: rgba(216, 111, 146, 0.45);
+}
+
+.page-division img {
+  width: 50%;
+  display: block;
+  margin: 0 auto;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import './assets/main.css'
-
+import 'primevue/resources/themes/lara-light-blue/theme.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
 
+app.use(PrimeVue)
 app.use(createPinia())
 app.use(router)
 
