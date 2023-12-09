de7c3b3cd8f68e5caebaabdfe709d4ca8911614e
WeepingDogel <weepingdogel@gmail.com>
2023-12-09 21:48:10 +0800

    Login Page, data tye of room and room_list.

--- src/components/Auth/AuthPage.vue
+++ src/components/Auth/AuthPage.vue
@@ -3,9 +3,39 @@ export default {};
 </script>
 
 <template>
-
+  <div class="login-page-container">
+    <div class="login-register-box">
+      <h1 class="login-register-page-title title-text">Sign In</h1>
+      <div class="box-form">
+        <input />
+        <input />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.login-page-container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  animation: FadeIn 450ms;
+}
 
+.login-register-box {
+  width: 800px;
+  height: 600px;
+  background-color: #f1f1f1;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.login-register-page-title {
+  font-size: 50px;
+  text-align: center;
+}
 </style>
--- src/components/Home/HomePage.vue
+++ src/components/Home/HomePage.vue
@@ -1,10 +1,33 @@
 <script lang="ts">
-export default {};
+import axios from "axios";
+
+type ServerStatus = {};
+
+type Room = {
+  
+};
+type RoomList = Room[];
+
+export default {
+  data() {
+    return {
+      RoomList: [] as RoomList
+    };
+  },
+  methods: {
+    async GetRoomList() {
+      const response = await axios.get<RoomList>("/api/");
+      this.RoomList = response.data;
+    },
+  },
+};
 </script>
 
 <template>
   <div class="home-page-container">
-    <p class="home-page-top-text title-text">A web game like werewolf, but nsfw.</p>
+    <p class="home-page-top-text title-text">
+      A web game like werewolf, but nsfw.
+    </p>
   </div>
 </template>
 
